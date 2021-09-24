5c2e9c191d285ab0551d82f75bf1e84b597fc619
0cddo <0.c.dddo@gmail.com>
2021-09-24 16:40:14 +0900

    Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-// import { current } from 'immer';
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     // movie array
     movies: []
+  };
+   
+  // async await 사용
+  getMovies = async() => {
+    // data fetch위해 axios 사용 (fetch 위에 있는 layer? )
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
   }
-  // 시작: component mount됨 (5초 뒤에 실행)
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false, book: true});
-    }, 5000)
-  }
+    // fetch() -> javascript 에서 data fetch 하는 법
+    this.getMovies();
+  };
 
   render() { 
     const {isLoading} = this.state
